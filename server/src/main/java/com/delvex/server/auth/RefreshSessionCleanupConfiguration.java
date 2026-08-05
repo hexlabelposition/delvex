c5f19d8c1bdb6752252b0c68aebff33c43579404
Vladyslav Kudriashov <129685708+hexlabelposition@users.noreply.github.com>
2026-08-06 01:48:20 +0200

    feat: enable refresh session cleanup

---
+++ server/src/main/java/com/delvex/server/auth/RefreshSessionCleanupConfiguration.java
@@ -0,0 +1,9 @@
+package com.delvex.server.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class RefreshSessionCleanupConfiguration {
+}

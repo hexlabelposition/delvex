c91a94bf0c31acf7588b7bfcd932417bcc55611e
Vladyslav Kudriashov <129685708+hexlabelposition@users.noreply.github.com>
2026-08-21 18:20:27 +0200

    fix: hide customer role from profile

--- client/src/_pages/profile/ui/profile-page.tsx
+++ client/src/_pages/profile/ui/profile-page.tsx
@@ -127,7 +127,6 @@ export function ProfilePage() {
             <dl className="mt-5 grid gap-x-12 gap-y-5 text-sm sm:grid-cols-2">
               <Info label="User ID" value={user.id} mono />
               <Info label="Email" value={user.email} />
-              <Info label="Role" value={user.role} />
               <Info label="First name" value={user.firstName} />
               <Info label="Last name" value={user.lastName} />
               <Info
